4dee47233ba52ea7b7194e7583da60026c3d39e9
rhythmos <ryancmoses@outlook.com>
2025-06-06 14:57:28 -0400

    CFA-38 added routes and database packages

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 import express from "express";
-import 'dotenv/config';
+import bodyParser from "body-parser";
+import "dotenv/config";
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get("/", () => {
+
+});
+app.post("/", () => {});
+
 app.listen(PORT, () => {
-    console.log(`server running on port ${PORT}`);
+  console.log(`server running on port ${PORT}`);
 });
